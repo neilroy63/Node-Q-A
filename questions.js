2c3b651c9875dc1997fea2b93555e6389d3e8d7a
Neil <neil.roy63@gmail.com>
2021-05-15 18:13:34 +1000

    fix/added process features

--- questions.js
+++ questions.js
@@ -1,8 +1,8 @@
 const questions = [
-    "What is your name?",
-    "What do you like doing?",
-    "What would you rather be doing?"
-]
+  "What is your name?",
+  "What do you like doing?",
+  "What type of coding are you working on?",
+];
 
 const ask = (i=0) => {
     process.stdout.write(`\n\n\n ${questions[i]}`);
@@ -10,3 +10,26 @@ const ask = (i=0) => {
 }
 
 ask()
+
+const answers = [];
+process.stdin.on("data", (data) => {
+  answers.push(data.toString().trim());
+
+  if (answers.length < questions.length) {
+    ask(answers.length);
+  } else {
+    process.exit();
+  }
+});
+
+process.on("exit", () => {
+  const [name, hobby, task] = answers;
+  console.log(`
+
+    Thank you for your answers!
+
+    Go ${hobby} ${name}, you can ${task} later!
+
+    
+    `);
+});
