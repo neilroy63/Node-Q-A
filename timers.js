486b261dd6ec41c7aed80210ab17a30dcc2a801e
Neil <neil.roy63@gmail.com>
2021-05-15 19:05:09 +1000

    feature/added set interval function

--- timers.js
+++ timers.js
@@ -1,21 +1,24 @@
-const waitTime = 3000;
+const waitTime = 5000;
 const waitInterval = 500;
 let currentTime = 0;
 
 // increment time function
 const incTime = () => {
-    currentTime += waitInterval
-    console.log(`waiting ${currentTime / 1000} seconds`)
-}
+  currentTime += waitInterval;
+  const p = Math.floor((currentTime  /  waitTime) * 100);;
+  process.stdout.clearLine();;
+  process.stdout.cursorTo(0);;
+  process.stdout.write(`waiting ... ${p} %`);;
+};
 
-console.log(`setting a ${waitTime/1000} second delay`)
+console.log(`setting a ${waitTime / 1000} second delay`);
 
 const timerFinished = () => {
-    clearInterval(interval)
-    console.log("done")
-}
+  clearInterval(interval);
+  process.stdout.clearLine();;
+  process.stdout.cursorTo(0);;
+  console.log("done");
+};
 
-
-const interval = setInterval(incTime, waitInterval)
+const interval = setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
-
